3a6670aa8a97e6f11a975c213f02d171c8e50157
Paulo Sergio <pauloserg861@gmail.com>
2024-02-12 21:17:35 -0300

    feat: implement guards

---
+++ src/modules/auth/guards/roles-auth.guard.ts
@@ -0,0 +1,33 @@
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { AuthRequest } from '../models/auth.request';
+import { JwtPayload } from '../models/jwt.decode';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request: AuthRequest = context.switchToHttp().getRequest();
+    const jwtToken = request.headers.authorization;
+
+    const decode = jwtToken.split('.')[1];
+    const decodeBuffer = Buffer.from(decode, 'base64');
+    const role = JSON.parse(decodeBuffer.toString()) as JwtPayload;
+
+    const rolesUser = role.user.role;
+
+    if (rolesUser === 'admin') {
+      return true;
+    } else {
+      throw new UnauthorizedException(
+        'Esta requisição só poderá ser feita por um usuário administrativo',
+      );
+    }
+  }
+}
