5b74759584da55de9bc384146d53bf46800f7d01
Paulo Sergio <pauloserg861@gmail.com>
2024-02-13 10:49:37 -0300

    feat: config swagger into application

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 const logger = new Logger('main.ts');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('ProDescriber API')
+    .setDescription(
+      'ProDescriber API is an api for generating descriptions for products to bust sales.',
+    )
+    .setVersion('1.0')
+    .addTag('prodescriber')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.enableCors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
