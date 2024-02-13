a474cabb18b98bcd6a4d82d548803ba268f8064c
Paulo Sergio <pauloserg861@gmail.com>
2024-02-12 21:17:06 -0300

    feat: enable api cors and remake logs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+
+const logger = new Logger('main.ts');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3003);
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  });
+  await app
+    .listen(3003)
+    .then(() =>
+      logger.log('[PRO_DESCRIBER_API] is running on http://localhost:3003'),
+    );
 }
 bootstrap();
