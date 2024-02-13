398f218db9f6c3cce729dac1b82e5e83b1efd31c
Paulo Sergio <pauloserg861@gmail.com>
2024-02-13 14:02:47 -0300

    feat: add get me verification to authenticated user

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,21 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { IsPublic } from './modules/auth/decorators/is-public.decorator';
+import { UserEntity } from './modules/users/domain/entities/User';
+import { CurrentUser } from './modules/auth/decorators/currentuser.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @IsPublic()
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('me')
+  getMe(@CurrentUser() user: UserEntity) {
+    return user;
   }
 }
