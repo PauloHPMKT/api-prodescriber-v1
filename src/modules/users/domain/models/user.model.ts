537b5faf01d942afed38b10158d265989020c82e
Paulo Sergio <pauloserg861@gmail.com>
2024-02-12 21:26:56 -0300

    feat: add property for userModel

--- src/modules/users/domain/models/user.model.ts
+++ src/modules/users/domain/models/user.model.ts
@@ -1,5 +1,8 @@
 import { UserEntity } from '../entities/User';
 
 export namespace UserModel {
-  export type ToCreate = Omit<UserEntity, 'id' | 'created_at' | 'updated_at'>;
+  export type ToCreate = Omit<
+    UserEntity,
+    'id' | 'avatar' | 'status' | 'created_at' | 'updated_at'
+  >;
 }
