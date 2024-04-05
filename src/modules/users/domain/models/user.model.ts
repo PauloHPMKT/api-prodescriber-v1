1d2ac9cf6a229e2822372ad1b2a95f55df4a6911
Paulo Sergio <pauloserg861@gmail.com>
2024-04-05 20:24:56 -0300

    feat: set props to be omited

--- src/modules/users/domain/models/user.model.ts
+++ src/modules/users/domain/models/user.model.ts
@@ -3,6 +3,12 @@ import { UserEntity } from '../entities/User';
 export namespace UserModel {
   export type ToCreate = Omit<
     UserEntity,
-    'id' | 'avatar' | 'status' | 'created_at' | 'updated_at'
+    | 'id'
+    | 'avatar'
+    | 'plan'
+    | 'role_system'
+    | 'status'
+    | 'created_at'
+    | 'updated_at'
   >;
 }
