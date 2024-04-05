e0c5915f58b1e15390ddca33ac643b03e767f8da
Paulo Sergio <pauloserg861@gmail.com>
2024-04-05 20:17:24 -0300

    feat: implement create user usecase

---
+++ src/modules/users/application/usecases/update-user.usecase.ts
@@ -0,0 +1,30 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { UpdateUserRepository } from '../../infra/repositories/update-user.repository';
+import { UpdateUserDto } from '../../presentation/dtos/update-user.dto';
+import { VerifyUserRepository } from '../../infra/repositories/verify-user.repository';
+
+@Injectable()
+export class UpdateUserUseCase {
+  constructor(
+    @Inject('UpdateUserRepository')
+    private readonly updateUserRepository: UpdateUserRepository,
+    @Inject('VerifyUserRepository')
+    private readonly verifyUserRepository: VerifyUserRepository,
+  ) {}
+  async update(id: string, data: UpdateUserDto): Promise<any> {
+    console.log(id, data);
+    // const requiredFields = Object.keys(data).filter(
+    //   (field) => field !== 'email',
+    // );
+    // for (const field of requiredFields) {
+    //   if (!data[field]) {
+    //     throw new Error(`Campo ${field} é obrigatório`);
+    //   }
+    // }
+    // const existUser = await this.verifyUserRepository.verify({ id });
+    // if (!existUser) {
+    //   throw new Error('User not found');
+    // }
+    // return await this.updateUserRepository.update(id, data);
+  }
+}
